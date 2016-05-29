88841a76bcb826786326ab57046a6099314f5a85
Alan Szlosek <alan.szlosek@gmail.com>
2016-05-29 14:34:48 -0400

    updated file to mysql migration script

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -4,45 +4,55 @@ var zlib = require('zlib');
 var config = require('../config');
 
 var mysqlPool = mysql.createPool(config.mysql);
-var folderPath = '../chunks/test';
+var folderPath = '../chunks/coders';
 
 fs.readdir(folderPath, function(error, files) {
-    for (var i = 0; i < files.length; i++) {
 
-        var closure = function(filename) {
-            fs.readFile(folderPath + '/' + filename, function(err, data) {
-                if (err) {
-                    console.log('failed to read file ' + filename);
+    var batch = function() {
+        for (var i = 0; i < 5; i++) {
+            if (files.length > 0) {
+                closure(files.pop());
+            }
+        }
+        if (files.length > 0) {
+            setTimeout(batch, 100);
+        }
+        
+    };
+
+    var closure = function(filename) {
+        fs.readFile(folderPath + '/' + filename, function(err, data) {
+            if (err) {
+                console.log('failed to read file ' + filename);
+                return;
+            }
+            var position = filename.replace(/n/g, '-').split('.').map(function(value) {
+                return Number(value);
+            });
+
+            zlib.gzip(data, function(error, buffer) {
+                if (error) {
+                    console.log('Error compressing voxels', error);
                     return;
                 }
-                var position = filename.replace(/n/g, '-').split('.').map(function(value) {
-                    return Number(value);
-                });
-
-                zlib.gzip(data, function(error, buffer) {
-                    if (error) {
-                        console.log('Error compressing voxels', error);
-                        return;
-                    }
-                    mysqlPool.query(
-                        'REPLACE INTO chunk SET ?',
-                        {
-                            x: position[0],
-                            y: position[1],
-                            z: position[2],
-                            voxels: buffer,
-                            updated_ms: Date.now()
-                        },
-                        function(error) {
-                            if (error) {
-                                console.log('MysqlChunkStore::saveVoxels', error);
-                            }
+                mysqlPool.query(
+                    'REPLACE INTO chunk SET ?',
+                    {
+                        x: position[0],
+                        y: position[1],
+                        z: position[2],
+                        voxels: buffer,
+                        updated_ms: Date.now()
+                    },
+                    function(error) {
+                        if (error) {
+                            console.log('MysqlChunkStore::saveVoxels', error);
                         }
-                    );
-                });
+                    }
+                );
             });
-        };
-        closure(files[i]);
+        });
+    };
 
-    }
+    batch();
 });
