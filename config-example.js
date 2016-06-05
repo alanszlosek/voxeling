df12320e6fb6b9fe4f1f14b3c8c0eaaab5f9ddd1
Alan Szlosek <alan.szlosek@gmail.com>
2016-06-05 13:44:19 -0400

    testbdcraft textures

--- config-example.js
+++ config-example.js
@@ -5,8 +5,9 @@ module.exports = {
 
     initialPosition: [16.5, 25.5, 16.5],
     worldRadius: 10,
-    chunkFolder: './chunks/test/',
+    chunkFolder: 'chunks/test/',
     mysql: {
+        connectionLimit: 10,
         user: 'voxeling',
         password: 'voxeling',
         database: 'voxeling',
@@ -15,7 +16,7 @@ module.exports = {
     server: 'ws://127.0.0.1:10005',
     websocketBindAddress: '127.0.0.1',
     websocketBindPort: 10005,
-    maxPlayers: 2,
+    maxPlayers: 10,
 
     textures: [
         {
@@ -23,127 +24,242 @@ module.exports = {
             name: 'grass+dirt',
             sides: [
                 14,
-                23, 23, 23, 23,
+                302, 302, 302, 302,
                 3
             ]
         },
         {
             value: 14,
             name: 'grass',
-            src: '/textures/grass.png'
+            src: '/testbdcraft/default_grass.png'
         },
         {
             value: 17,
-            name: 'long grass',
-            src: '/textures/long_grass.png'
+            name: 'grass2',
+            src: '/testbdcraft/default_grass_footsteps.png'
         },
         {
             value: 3,
             name: 'dirt',
-            src: '/textures/dirt.png'
+            src: '/testbdcraft/default_dirt.png'
         },
         {
-            value: 23,
+            value: 302,
             name: 'grass_dirt',
-            src: '/textures/grass_dirt.png',
+            src: '/testbdcraft/default_grass_side.png',
             hidden: true
         },
 
         {
             value: 2,
             name: 'brick',
-            src: '/textures/brick.png'
+            src: '/testbdcraft/default_brick.png'
         },
         {
             value: 9,
             name: 'brick2',
-            src: '/textures/brick2.png'
+            src: '/testbdcraft/default_brick.png',
+            hidden: true
         },
         {
             value: 10,
             name: 'brick3',
-            src: '/textures/brick3.png'
+            src: '/testbdcraft/default_brick.png',
+            hidden: true
         },
 
         {
             value: 4,
-            name: 'obsidian',
-            src: '/textures/obsidian.png'
+            name: 'coal',
+            src: '/testbdcraft/default_mineral_coal.png'
         },
         {
             value: 13,
-            name: 'granite',
-            src: '/textures/granite.png'
+            name: 'iron',
+            src: '/testbdcraft/default_mineral_iron.png'
         },
         {
             value: 19,
-            name: 'slate',
-            src: '/textures/slate.png'
+            name: 'clay',
+            src: '/testbdcraft/default_clay.png'
         },
         {
             value: 18,
             name: 'sandstone',
-            src: '/textures/sandstone.png'
+            src: '/testbdcraft/default_sandstone.png'
         },
         {
             value: 11,
-            name: 'cobblestone',
-            src: '/textures/cobblestone.png'
+            name: 'cobble',
+            src: '/testbdcraft/default_cobble.png'
         },
         {
             value: 12,
-            name: 'cobblestone2',
-            src: '/textures/cobblestone2.png'
+            name: 'cobble2',
+            src: '/testbdcraft/default_cobble.png',
+            hidden: true
         },
         {
             value: 15,
-            name: 'grassy cobblestone',
-            src: '/textures/grassy_cobblestone.png'
+            name: 'moss cobble',
+            src: '/testbdcraft/default_mossycobble.png'
         },
 
         {
             value: 7,
             name: 'lava',
-            src: '/textures/lava.png'
+            src: '/testbdcraft/default_lava.png'
         },
         
         {
             value: 6,
             name: 'water',
-            src: '/textures/water.png'
+            src: '/testbdcraft/default_water.png'
         },
         {
             value: 16,
             name: 'ice',
-            src: '/textures/ice.png'
+            src: '/textures/ice.png',
+            hidden: true
         },
         {
             value: 20,
             name: 'snow',
-            src: '/textures/sparkly_snow.png'
-        },
-        {
-            value: 5,
-            name: 'whitewool',
-            src: '/textures/whitewool.png'
+            src: '/textures/sparkly_snow.png',
+            hidden: true
         },
 
         {
             value: 8,
-            name: 'crate',
-            src: '/textures/crate.png'
+            name: 'chest',
+            sides: [
+                300,
+                301, 301, 301, 301,
+                301
+            ]
+        },
+        {
+            value: 300,
+            name: 'chest top',
+            src: '/testbdcraft/default_chest_top.png',
+            hidden: true
+        },
+        {
+            value: 301,
+            name: 'chest side',
+            src: '/testbdcraft/default_chest_side.png',
+            hidden: true
         },
         
         {
-            value: 21,
-            name: 'straw',
-            src: '/textures/straw.png'
-        },
-        {
             value: 22,
             name: 'wood',
-            src: '/textures/woodsiding.png'
+            src: '/testbdcraft/default_wood.png'
         },
+        {
+            value: 24,
+            name: 'tree',
+            sides: [
+                303,
+                304, 304, 304, 304,
+                303
+            ]
+        },
+        {
+            value: 303,
+            name: 'tree_top',
+            src: '/testbdcraft/default_tree_top.png',
+            hidden: true
+        },
+        {
+            value: 304,
+            name: 'tree_side',
+            src: '/testbdcraft/default_tree.png',
+            hidden: true
+        },
+        {
+            value: 100,
+            name: 'leaves',
+            src: '/testbdcraft/default_leaves.png'
+        },
+
+        {
+            value: 27,
+            name: 'black wool',
+            src: '/testbdcraft/wool_black.png'
+        },
+        {
+            value: 34,
+            name: 'grey wool',
+            src: '/testbdcraft/wool_grey.png'
+        },
+        {
+            value: 32,
+            name: 'dk grey wool',
+            src: '/testbdcraft/wool_dark_grey.png'
+        },
+        {
+            value: 29,
+            name: 'brown wool',
+            src: '/testbdcraft/wool_brown.png'
+        },
+        {
+            value: 28,
+            name: 'blue wool',
+            src: '/testbdcraft/wool_blue.png'
+        },
+        {
+            value: 30,
+            name: 'cyan wool',
+            src: '/testbdcraft/wool_cyan.png'
+        },
+        {
+            value: 33,
+            name: 'green wool',
+            src: '/testbdcraft/wool_green.png'
+        },
+        {
+            value: 31,
+            name: 'dk green wool',
+            src: '/testbdcraft/wool_dark_green.png'
+        },
+        {
+            value: 35,
+            name: 'magenta wool',
+            src: '/testbdcraft/wool_magenta.png'
+        },
+        {
+            value: 39,
+            name: 'violet wool',
+            src: '/testbdcraft/wool_violet.png'
+        },
+        {
+            value: 37,
+            name: 'pink wool',
+            src: '/testbdcraft/wool_pink.png'
+        },
+        {
+            value: 38,
+            name: 'red wool',
+            src: '/testbdcraft/wool_red.png'
+        },
+        {
+            value: 21,
+            name: 'yellow wool',
+            src: '/testbdcraft/wool_yellow.png'
+        },
+        {
+            value: 36,
+            name: 'orange wool',
+            src: '/testbdcraft/wool_orange.png'
+        },
+        {
+            value: 5,
+            name: 'white wool',
+            src: '/testbdcraft/wool_white.png'
+        },
+
+
 
         {
             value: 50,
@@ -162,17 +278,7 @@ module.exports = {
             name: 'viking',
             src: '/textures/viking.png',
             hidden: true
-        },
-        
-        {
-            value: 100,
-            name: 'leaves',
-            src: '/textures/leaves_oak.png'
-        },
-        {
-            value: 101,
-            name: 'glass',
-            src: '/textures/glass.png'
         }
+        
     ]
 }
