41cc2851ee038e9f9ba341b5ee3e7bde8312294c
Alan Szlosek <alan.szlosek@gmail.com>
2016-11-25 20:43:02 -0500

    physics: climb single blocks without having to jump

--- src/lib/physics.js
+++ src/lib/physics.js
@@ -5,24 +5,11 @@ var glm = require('gl-matrix'),
     quat = glm.quat;
 var raycast = require('voxel-raycast');
 
-/*
-- Need to make a bounding box for our character
-- For each vector direction that we're moving (non-zero):
-    - Check the two lines on the side of the face that faces the direction. if moving forward, check vertical lines on the left and right against nearby voxels
-*/
-var pos = function(data) {
-    return data[0] + ', ' + data[1] + ', ' + data[2];
-};
-
-var t = "\t";
 var debug = false;
 // ticks per second
 var tps = 60;
 // this gets added to, or subtracted, based on deltaAcceleration
-var currentVelocity = vec3.create();
-var camera = mat4.create();
-// positionVector
-var voxel = 0;
+
 
 // each is half of what we really want
 var accelerations = {
@@ -47,12 +34,12 @@ function Physics(movable, controlState, game) {
     this.controlState = controlState;
     this.movable = movable;
     this.game = game;
-    // temp variables
+
+    this.currentVelocity = vec3.create();
     this.rotatedMovementVector = vec3.create();
     this.rotationQuat = quat.create();
-    this.newPosition = vec3.create();
-    this.direction = vec3.create();
-    this.adjustedDirection = vec3.create();
+    this.previousVelocity = vec3.create();
+
 }
 
 Physics.prototype.tick = function() {
@@ -61,87 +48,86 @@ Physics.prototype.tick = function() {
     // don't tick based on delta milliseconds ... lets always assume our tick rate:
     // much less math, and if we have a pause, the character won't lurch forward
     if (this.controlState.forward == 1) {
-        currentVelocity[2] += -accelerations.walk;
-        currentVelocity[2] = Math.max(currentVelocity[2], -velocities.maxWalk);
+        this.currentVelocity[2] += -accelerations.walk;
+        this.currentVelocity[2] = Math.max(this.currentVelocity[2], -velocities.maxWalk);
     } else if (this.controlState.forward > 0) {
-        currentVelocity[2] = -velocities.maxWalk * this.controlState.forward;
+        this.currentVelocity[2] = -velocities.maxWalk * this.controlState.forward;
     } else if (this.controlState.backward == 1) {
-        currentVelocity[2] += accelerations.walk;
-        currentVelocity[2] = Math.min(currentVelocity[2], velocities.maxWalk);
+        this.currentVelocity[2] += accelerations.walk;
+        this.currentVelocity[2] = Math.min(this.currentVelocity[2], velocities.maxWalk);
     } else if (this.controlState.backward > 0) {
-        currentVelocity[2] = velocities.maxWalk * this.controlState.backward;
+        this.currentVelocity[2] = velocities.maxWalk * this.controlState.backward;
     } else {
         // Slowdown
-        if (currentVelocity[2] > 0) {
-            currentVelocity[2] -= accelerations.slowdown;
-            if (currentVelocity[2] < 0) {
-                currentVelocity[2] = 0;
+        if (this.currentVelocity[2] > 0) {
+            this.currentVelocity[2] -= accelerations.slowdown;
+            if (this.currentVelocity[2] < 0) {
+                this.currentVelocity[2] = 0.00;
             }
-        } else if (currentVelocity[2] < 0) {
-            currentVelocity[2] += accelerations.slowdown;
-            if (currentVelocity[2] > 0) {
-                currentVelocity[2] = 0;
+        } else if (this.currentVelocity[2] < 0) {
+            this.currentVelocity[2] += accelerations.slowdown;
+            if (this.currentVelocity[2] > 0) {
+                this.currentVelocity[2] = 0.00;
             }
         }
     }
     if (this.controlState.left == 1) {
         // keyboard, accelerate gradually
-        currentVelocity[0] += -accelerations.walk;
-        currentVelocity[0] = Math.max(currentVelocity[0], -velocities.maxWalk);
+        this.currentVelocity[0] += -accelerations.walk;
+        this.currentVelocity[0] = Math.max(this.currentVelocity[0], -velocities.maxWalk);
     } else if (this.controlState.left > 0) {
         // gamepad, no acceleration, use stick percentage
-        currentVelocity[0] = -velocities.maxWalk * this.controlState.left;
+        this.currentVelocity[0] = -velocities.maxWalk * this.controlState.left;
     } else if (this.controlState.right == 1) {
-        currentVelocity[0] += accelerations.walk;
-        currentVelocity[0] = Math.min(currentVelocity[0], velocities.maxWalk);
+        this.currentVelocity[0] += accelerations.walk;
+        this.currentVelocity[0] = Math.min(this.currentVelocity[0], velocities.maxWalk);
     } else if (this.controlState.right > 0) {
-        currentVelocity[0] = velocities.maxWalk * this.controlState.right;
+        this.currentVelocity[0] = velocities.maxWalk * this.controlState.right;
     } else {
         // Slowdown
-        if (currentVelocity[0] > 0) {
-            currentVelocity[0] -= accelerations.slowdown;
-            if (currentVelocity[0] < 0) {
-                currentVelocity[0] = 0;
+        if (this.currentVelocity[0] > 0) {
+            this.currentVelocity[0] -= accelerations.slowdown;
+            if (this.currentVelocity[0] < 0) {
+                this.currentVelocity[0] = 0.00;
             }
-        } else if (currentVelocity[0] < 0) {
-            currentVelocity[0] += accelerations.slowdown;
-            if (currentVelocity[0] > 0) {
-                currentVelocity[0] = 0;
+        } else if (this.currentVelocity[0] < 0) {
+            this.currentVelocity[0] += accelerations.slowdown;
+            if (this.currentVelocity[0] > 0) {
+                this.currentVelocity[0] = 0.00;
             }
         }
     }
     
     // flying and jumping should fall slowly
-    if (this.controlState.jump && currentVelocity[1] == 0) {
+    if (this.controlState.jump && this.currentVelocity[1] == 0) {
         // only allow jumping if we're on the ground
         slowFall = true;
-        currentVelocity[1] = velocities.jump;
+        this.currentVelocity[1] = velocities.jump;
     } else if (this.controlState.fly) {
         slowFall = true;
         // dont exceed maximum upward velocity
-        currentVelocity[1] += accelerations.fly;
-        if (currentVelocity[1] > 0) {
-            currentVelocity[1] = Math.min(currentVelocity[1], velocities.maxFly);
+        this.currentVelocity[1] += accelerations.fly;
+        if (this.currentVelocity[1] > 0) {
+            this.currentVelocity[1] = Math.min(this.currentVelocity[1], velocities.maxFly);
         }
     } else {
-        if (slowFall && currentVelocity[1] < 0) {
-            currentVelocity[1] += accelerations.partialGravity;
+        if (slowFall &&this.currentVelocity[1] < 0) {
+            this.currentVelocity[1] += accelerations.partialGravity;
         } else {
-            currentVelocity[1] += accelerations.gravity;
+            this.currentVelocity[1] += accelerations.gravity;
         }
     }
-    this.handleCollision(currentVelocity);
+    this.handleCollision(this.currentVelocity);
 };
 
 Physics.prototype.handleCollision = function(movementVector) {
     var self = this;
-    var hit = [ 0, 0, 0 ], normals = [ 0, 0, 0 ];
-    var len;
-    var bounds = this.movable.bounds.all;
     var currentPosition = this.movable.getPosition();
-    var direction = this.direction;
-    var adjustment;
-    var collision;
+    var testPosition = vec3.create();
+    var direction = vec3.create();
+    var direction2 = vec3.create();
+    var hit = vec3.create();
+    var normals = vec3.create();
 
     var boundsMap = {
         p0: 'front',
@@ -152,71 +138,86 @@ Physics.prototype.handleCollision = function(movementVector) {
         n2: 'left'
     };
 
-    var log = function(num, start, direction, len, hit, normals, adjustment) {
-        if (debug) {
-            console.log('  start' + num + ':', pos(start));
-            console.log('    direction: ', pos(direction));
-            console.log('    len: ', len);
-            console.log('    hit: ', pos(hit));
-            console.log('    normals: ', pos(normals));
-            console.log('    adjusted: ', adjustment);
-        }
-    };
     
-
-    this.movable.updateBounds(currentPosition);
-
     quat.identity(this.rotationQuat);
     quat.rotateY(this.rotationQuat, this.rotationQuat, self.movable.getYaw());
+    vec3.transformQuat(direction, movementVector, this.rotationQuat);
 
-    vec3.transformQuat(this.rotatedMovementVector, movementVector, this.rotationQuat);
-    // newPosition holds updated object position
-    vec3.add(this.newPosition, currentPosition, this.rotatedMovementVector);
-    // use updated position to calculate direction vector
-    vec3.subtract(direction, this.newPosition, currentPosition);
-    // TODO: debug direction ... seems wrong, as some points aren't triggering a colllision as expected
 
-    // Raycast for as many directions as we're moving in, from every bounding box point that faces those directions
-    for (var axis = 0; axis < 3; axis++) {
-        var val = direction[axis];
-        if (val == 0) {
-            continue;
-        }
-        len = Math.sqrt(Math.pow(direction[0], 2) + Math.pow(direction[1], 2) + Math.pow(direction[2], 2));
+    // Try to step up, but only if we're on the ground
+    if (this.controlState.forward && this.previousVelocity[1] == 0.00) {
+        vec3.copy(testPosition, currentPosition);
+        testPosition[1] += 0.8;
+        var collision = raycast(self.game, testPosition, direction, 0.5, hit, normals);
+        
+        testPosition[1] += 0.2;
 
-        // This isn't quite working right yet
-        /*
-        // We don't want to raycast from all bounding box points, just the ones that point where we're moving
-        bounds = this.movable.bounds[
-            boundsMap[
-                (val > 0 ? 'p' : 'n') + axis
-            ]
-        ];
-        */
-        for (var i = 0; i < bounds.length; i++) {
-            var start = bounds[i];
-            raycast(self.game, start, direction, len, hit, normals);
+        if (
+            collision
+            &&
+            self.game.getBlock(testPosition[0], testPosition[1], testPosition[2]) == 0
+        ) {
+            currentPosition[1] += 1.2;
 
-            if (normals[axis] != 0) {
-                adjustment = hit[axis] - start[axis];
-                if (normals[axis] > 0) {
-                    adjustment = Math.ceil(adjustment);
-                } else {
-                    adjustment = Math.floor(adjustment);
-                }
-                log(1, start, direction, len, hit, normals, adjustment);
-                direction[axis] = adjustment;
-                currentVelocity[axis] = 0;
-                if (axis == 1) {
-                    slowFall = false;
-                }
-                // Break out after a collision on one of the bounding box points
-                break;
+            this.movable.updateBounds(currentPosition);
+            vec3.copy(direction2, direction);
+
+            // Did we have any significant collisions? If so, roll back the Y-axis change
+            if (this.haggle(this.movable.bounds.all, currentPosition, direction2)) {
+                currentPosition[1] -= 1.2;
+            } else {
+                vec3.copy(this.previousVelocity, direction2);
+                self.movable.translate(direction2);
+                return;
             }
         }
     }
 
+    this.movable.updateBounds(currentPosition);
+    this.haggle(this.movable.bounds.all, currentPosition, direction);
+    vec3.copy(this.previousVelocity, direction);
     self.movable.translate(direction);
+};
+
+// Haggle for a stable, non-colliding position
+// Result of this is that direction vec3 gets adjusted in the process
+Physics.prototype.haggle = function(bounds, start, direction) {
+    var self = this;
+    var collided = false;
+    var len = vec3.length(direction);
+    var hit = vec3.create();
+    var normals = vec3.create();
+
+    for (var i = 0; i < bounds.length; i++) {
+        var start = bounds[i];
+        var adjusted = false;
+
+        // If we've already adjusted the direction to 0 (like when we're up against a wall), skip further dection
+        if (len == 0) {
+            break;
+        }
+        var collision = raycast(self.game, start, direction, len, hit, normals);
+
+        // Back off direction up to collision point along collision surface normals
+        for (var axis = 0; axis < 3; axis++) {
+            if (normals[axis] < 0.00 || 0.00 < normals[axis]) {
+                adjusted = true;
+                // Snap to voxel boundary upon collision
+                if (normals[axis] > 0) {
+                    direction[axis] = Math.ceil(hit[axis] - start[axis]);
+                } else {
+                    direction[axis] = Math.floor(hit[axis] - start[axis]);
+                }
+
+               this.currentVelocity[axis] = 0.00;
+            }
+        }
+        if (adjusted) {
+            collided = true;
+            len = vec3.length(direction);
+        }
+    }
+    return collided;
 };
 
 module.exports = Physics;
