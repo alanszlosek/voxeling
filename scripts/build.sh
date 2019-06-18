8a93917bdbd2a15f0fa916e617bf897634ba72bb
Alan Szlosek <alan.szlosek@gmail.com>
2019-06-18 07:46:35 -0400

    script to build bundle.js and webworker js

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+dir=`dirname $0`
+cd $dir/..
+node node_modules/browserify/bin/cmd.js src/client.js -o www/bundle.js
+node node_modules/browserify/bin/cmd.js src/client-worker.js -o www/client-worker.js
