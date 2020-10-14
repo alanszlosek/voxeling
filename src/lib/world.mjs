2dff4e77bcd70edee255900f85d554d78d669bc6
Alan Szlosek Jr <alan.szlosek@gmail.com>
2020-10-14 06:45:57 -0400

    show coords at top-right

--- src/lib/world.mjs
+++ src/lib/world.mjs
@@ -3,10 +3,22 @@ import { Tickable } from './entities/tickable';
 class World extends Tickable {
     constructor(game) {
         super();
+        let self = this;
         this.game = game;
 
         // start clearly out of bounds to trigger a load of world chunks
         this.lastRegion = [100,100,100];
+
+
+        let coords = document.getElementById('coordinates');
+        setInterval(
+            function() {
+                let player = self.game.player;
+                let pos = player.getPosition().map(Math.floor);
+                coords.innerText = pos.join(', ');
+            },
+            2000
+        );
     }
 
     tick(ts) {
@@ -14,7 +26,7 @@ class World extends Tickable {
         var thisRegion = this.game.coordinates.positionToChunk(this.game.player.position);
         var lastRegion = this.lastRegion;
         if (thisRegion[0] !== lastRegion[0] || thisRegion[1] !== lastRegion[1] || thisRegion[2] !== lastRegion[2]) {
-            console.log('change');
+            console.log('region change');
             this.game.clientWorkerHandle.regionChange();
         }
         this.lastRegion = thisRegion;
