453a944fb259c39b77dd1f9512ea2bf39419dd3e
Alan Szlosek Jr <alan.szlosek@gmail.com>
2021-02-20 17:30:53 -0500

    remove more old files

--- src/lib/voxel-stats.js
+++
@@ -1,8 +0,0 @@
-//var statsdClient = require('node-statsd-client').Client;
-//var statsd = new statsdClient('127.0.0.1', 8125);
-var prefix = 'voxeling.';
-
-module.exports = {
-    count: function(name) {},
-    timing: function(name, value) {}
-};
