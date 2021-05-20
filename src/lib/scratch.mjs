2787441e4f0a1d592e35712602ae5b95aff703e4
Alan Szlosek Jr <alan.szlosek@gmail.com>
2021-05-20 07:22:48 -0400

    more scratch types

--- src/lib/scratch.mjs
+++ src/lib/scratch.mjs
@@ -6,6 +6,8 @@ let scratch = {
     identityMat4: glm.mat4.create(),
     identityQuat: glm.quat.create(),
 
+    zeroVec3: glm.vec3.fromValues(0,0,0),
+
 // scratch mat4. can use this as the destination for calulations
     mat4: glm.mat4.create(),
     mat4_0: glm.mat4.create(),
@@ -15,7 +17,19 @@ let scratch = {
     quat_0: glm.quat.create(),
 
     vec3: glm.vec3.create(),
-    vec3_0: glm.vec3.create()
+    vec3_0: glm.vec3.create(),
+    vec3_1: glm.vec3.create(),
+    vec3_2: glm.vec3.create(),
+
+    f64vec3_0: new Float64Array(3),
+    f64vec3_1: new Float64Array(3),
+    f64vec3_2: new Float64Array(3),
+    f32vec3_0: new Float32Array(3),
+    f32vec3_1: new Float32Array(3),
+    f32ved3_2: new Float32Array(3),
+    i32vec3_0: new Int32Array(3),
+    i32vec3_1: new Int32Array(3),
+    i32vec3_2: new Int32Array(3),
 };
 
 export default scratch;
