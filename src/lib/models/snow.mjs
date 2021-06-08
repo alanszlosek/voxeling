420c5c957fde0747af7adefe8f0f8f996e484ef4
Alan Szlosek Jr <alan.szlosek@gmail.com>
2021-06-08 07:43:32 -0400

    snowflakes move as if on wind currents

--- src/lib/models/snow.mjs
+++ src/lib/models/snow.mjs
@@ -36,6 +36,12 @@ let scales = [
 ];
 let scaleIndex = 0;
 
+let speeds = [
+    vec3.fromValues(1.0, 1.0, 1.0),
+    vec3.fromValues(0.3, 0.6, 0.3),
+    vec3.fromValues(0.6, 1.0, 0.6)
+];
+
 let snowLayer = {};
 
 let newSnowpack = {};
@@ -72,15 +78,16 @@ converting layers to instancing
 
 
 class Snowflake extends Tickable {
-    constructor(game) {
+    constructor(game, velocity) {
         super();
         this.game = game;
         this.enabled = true;
-       
+        this.position = vec3.create();
+        this.velocity = vec3.create(); // current velocity
+        this.desiredVelocity = vec3.create(); // direction we'll lerp towards
         this.respawn();
     }
 
-    // TODO: this should return a vec3 already made
     getScale() {
         let scaleVector = scales[ scaleIndex ];
         scaleIndex++;
@@ -91,24 +98,29 @@ class Snowflake extends Tickable {
     }
 
     respawn() {
-        let position = this.game.player.getPosition();
+        let playerPosition = this.game.player.getPosition();
         this.scale = this.getScale();
-        this.fallSpeed = 0.06; // some range up to 0.1
+        this.speed = speeds[ Math.floor(Math.random() * speeds.length)];
         let spread = 32;
         // add 0.5 to center within block
-        this.position = vec3.fromValues(
-            getRandomInt(position[0] - spread, position[0] + spread) + 0.5,
-            getRandomInt(position[1] + 10, position[1] + 30),
-            getRandomInt(position[2] - spread, position[2] + spread) + 0.5
-        );
+        this.position[0] = getRandomInt(playerPosition[0] - spread, playerPosition[0] + spread) + 0.5;
+        this.position[1] = getRandomInt(playerPosition[1] + 10, playerPosition[1] + 30);
+        this.position[2] = getRandomInt(playerPosition[2] - spread, playerPosition[2] + spread) + 0.5;
+
         this.collisionCheck = 0;
+    }
+
+    updateVelocity(velocity) {
+        // scale snow-flake wide velocity based on this flake's relative speed to overall
+        vec3.multiply(this.desiredVelocity, velocity, this.speed);
     }
 
     tick(ts, delta) {
         if (!this.enabled) {
             return;
         }
-        this.position[1] -= this.fallSpeed;
+        vec3.lerp(this.velocity, this.velocity, this.desiredVelocity, 0.01);
+        vec3.add(this.position, this.position, this.velocity);
 
         // check for collision
         // If so, respawn flake and add another
@@ -116,12 +128,14 @@ class Snowflake extends Tickable {
         // Don't check for collision on every frame. Snowflakes fall slowly enough that we don't have to
         if (this.collisionCheck == 3) {
             if (this.game.voxelCache.getBlock(this.position[0], this.position[1], this.position[2]) > 0) {
+                // ignore collision if happened on side of cube ... how?
+
                 // log where snowflake collided as snowpack position
                 let pos = this.position.map(Math.floor);
                 let key = pos.join(',');
                 // tweak position to be top center of block
                 pos[0] += 0.5;
-                pos[1] += 1.1;
+                pos[1] += 1.001;
                 pos[2] += 0.5;
                 newSnowpack[ key ] = pos;
 
@@ -141,11 +155,14 @@ class Snow extends Renderable {
         // 1200 starts to stutter on GTX960
         this.numSnowflakes = 300;
         this.snowflakes = {};
+        this.velocity = vec3.create();
+        this.velocityChangeCutoff = 0; // we update velocity after N seconds
         this.enabled = true;
     }
     init() {
         for (let i = 0; i < this.numSnowflakes; i++) {
-            let sf = new Snowflake(this.game);
+            let sf = new Snowflake(this.game, this.velocity);
+            sf.enabled = this.enabled;
             this.snowflakes[ sf._tickableId ] = sf;
         }
 
@@ -190,8 +207,9 @@ class Snow extends Renderable {
     }
 
     initSnowpack() {
-        this.snowpackMemory = 2400;
+        this.snowpackMemory = 2400; // how many squares of snowpack to remember
         this.snowpackHelper = new Helper(this.snowpackMemory);
+        // TODO: change this back to vec3, and update instanced shader. can really save 3K+ of ram
         this.snowpackTranslations = new Float32Array( 4 * this.snowpackMemory );
 
         // position at 0,0,0
@@ -226,7 +244,7 @@ class Snow extends Renderable {
         // this is the main buffer that will drive instancing
         gl.bindBuffer(gl.ARRAY_BUFFER, buffers.translations);
         // snowpackMemory is places, but we need 4 floats for each position, and 4 bytes for each float
-        gl.bufferData(gl.ARRAY_BUFFER, 4 * 4 * this.snowpackMemory, gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, 4 * 4 * this.snowpackMemory, gl.DYNAMIC_DRAW);
 
         this.snowpackBuffers = buffers;
 
@@ -245,11 +263,35 @@ class Snow extends Renderable {
         
     }
 
-    tick() {
+    tick(ts) {
+        // update snowflake velocity
+        let vel = function() {
+            return (Math.random() - 0.5) / 10;
+        };
+        if (ts > this.velocityChangeCutoff) {
+            let fallSpeeds = [
+                -0.02,
+                -0.04,
+                -0.08
+            ];
+            this.velocity[0] = vel();
+            this.velocity[1] = fallSpeeds[ Math.floor(Math.random() * fallSpeeds.length) ];
+            this.velocity[2] = vel();
+
+            for (let _tickableId in this.snowflakes) {
+                let sf = this.snowflakes[ _tickableId ];
+                sf.updateVelocity(this.velocity);
+            }
+
+            this.velocityChangeCutoff = ts + getRandomInt(5000, 15000);
+        }
+
+        // bail if we don't have any snowpack to render
         let newMeshes = Object.keys(newSnowpack);
         if (newMeshes.length == 0) {
             return;
         }
+
         // make list of all meshes
         // merge new snow layer into existing
         // clear new layer
